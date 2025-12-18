9afed6103d3bbda2a0ec9eaf4dfc8c2032c81f63
deathehra <jarvidniies@gmail.com>
2025-12-18 15:59:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import psutil
 class Config:
     APP_ID = int(os.environ.get("APP_ID"))
     API_HASH = os.environ.get("API_HASH")
-    BASE_LANGUAGE = os.environ.get("BASE_LANGUAGE", default="en")
+    BASE_LANGUAGE = os.environ.get("BASE_LANGUAGE", "en")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
@@ -32,7 +32,7 @@ class Config:
     # Files under that size will not display a progress bar while uploading
     MIN_SIZE_PROGRESS = 1024 * 1024 * 50  # 50 MB
     MONGODB_URL = os.environ.get("MONGODB_URL")
-    MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", default="Unzipper_Bot")
+    MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", "Unzipper_Bot")
     TG_MAX_SIZE = 2097152000
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
-    VERSION = os.environ.get("UNZIPBOT_VERSION", default="7.3.0")
+    VERSION = os.environ.get("UNZIPBOT_VERSION", "7.3.0")
